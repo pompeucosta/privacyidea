2fcc381f3a1ab1bdc3f99799ddc19b3296bfe896
pompeucosta <pompeugcosta@gmail.com>
2025-04-08 15:00:15 +0100

    remove threshold database table

--- privacyidea/models.py
+++ privacyidea/models.py
@@ -947,30 +947,6 @@ class UserTypeRiskScore(MethodsMixin,db.Model):
         db.session.commit()
         return ret
     
-class ThresholdScore(MethodsMixin,db.Model):
-    __tablename__ = "thresholdriskscore"
-    id = db.Column(db.Integer(),Sequence("thresholdriskscore_seq"),primary_key=True)
-    token = db.Column(db.Unicode(50),nullable=False)
-    threshold = db.Column(db.Integer(),nullable=False)
-    
-    def __init__(self,token,threshold):
-        self.token = token
-        self.threshold = threshold
-        
-    def save(self):
-        ts = ThresholdScore.query.filter_by(token=self.token).first()
-        
-        if ts == None:
-            db.session.add(self)
-            db.session.commit()
-            ret = self.id
-        else:
-            ThresholdScore.query.filter_by(token=self.token).update({"threshold": self.threshold})
-            ret = ts.id
-            
-        db.session.commit()
-        return ret
-    
 class Admin(db.Model):
     """
     The administrators for managing the system.
